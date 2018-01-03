ec85c2a32a20580194b9a83739d67b1f3f580d66
Benjamin Coullie <ben.coullie@vendhq.com>
2018-01-04 07:38:26 +1300

    minor cleanup and add first react component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import React, { Component } from 'react'
+
+import SimpleAPI from './components/simpleAPI/simpleAPI.js'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='App'>
+        <SimpleAPI />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/components/simpleAPI/simpleAPI.js
@@ -0,0 +1,17 @@
+import './simpleAPI.css'
+
+import React, { Component } from 'react'
+
+class SimpleAPI extends Component {
+  render () {
+    return (
+      <div className='simpleAPI-wrapper'>
+        <div>
+          <h1>This should be centered</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SimpleAPI
